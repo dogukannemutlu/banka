9e8f4e3fbdb849d68e9096e23453a901f894cce3
nemutludogukan <nemutludogukan@hotmail.com>
2017-01-25 10:19:35 +0200

    Master

---
+++ DogukanNemutlu/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DogukanNemutlu.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Bankamız hakkında tüm bilgilere bu bölümden ulaşabilirsiniz.";
+            
+            return View();
+        }
+
+       
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "İletişim Bölümü";
+
+            return View();
+        }
+    }
+}
---
+++ DogukanNemutlu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DogukanNemutlu.MvcApplication" Language="C#" %>
